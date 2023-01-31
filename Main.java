f86fa535ca632db9d2455de90de05857f478ac58
Timurbek <timurbek.raxmanov@bk.ru>
2023-01-31 19:12:41 +0500

    Calculated created

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println(Hello wordl!);
+
+
+
+        System.out.println("Введите первое число");
+        long x = new Scanner(System.in).nextLong();
+        System.out.println("Введите второе число");
+        long y = new Scanner(System.in).nextLong();
+//        System.out.println("Введите действие");
+//        char  z = new Scanner(System.in).nextChar();
+        System.out.println(x+y);
     }
+
 }
